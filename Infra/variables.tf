fcfa095830c3b7c35590a80d0ec9445ad3f19956
iodine123 <57628364+iodine123@users.noreply.github.com>
2025-08-30 12:15:35 +0700

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,10 +1,1 @@
-variable "project_id" {
-  description = "sre-test-cloudmile-project"
-  type        = string
-}
 
-variable "region" {
-  description = "GCP region"
-  type        = string
-  default     = "asia-southeast1"
-}
