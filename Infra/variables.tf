4367353f7eb7340fe53aaefa15d89b0c5e62ae87
iodine123 <iodinehanifan@gmail.com>
2025-08-30 12:16:55 +0700

    Fix: addd vars

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {
+  description = "sre-test-cloudmile-project"
+  type        = string
+}
 
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "asia-southeast1"
+}
