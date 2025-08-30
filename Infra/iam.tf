e071d7e2de7923a4463fca818488c0db9ce3570c
iodine123 <iodinehanifan@gmail.com>
2025-08-30 17:48:08 +0700

    Fix: add IaM for container registry

--- Infra/iam.tf
+++ Infra/iam.tf
@@ -3,3 +3,9 @@ resource "google_project_iam_member" "sa_storage_admin" {
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
 }
+
+resource "google_project_iam_member" "gke_node_sa_ar_pull" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
+}
